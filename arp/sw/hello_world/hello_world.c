86a4a00ef81d3cd84c9158693d32debd9a4d3019
Valter Akira Miasato Filho <v.akira.miasato@gmail.com>
2014-06-06 11:41:58 -0300

    Modifica hello_world.c para inspecionar não-reentrância do printf

--- arp/sw/hello_world/hello_world.c
+++ arp/sw/hello_world/hello_world.c
@@ -2,9 +2,11 @@
 
 int main(int argc, char *argv[]){
   int i;
-  for(i=0;i<10;i++)
+  static int j = 1;
+  for(i=0;i<j;i++)
     printf("Hi from processor MIPS!\n");
-
+    printf("%d\n", j);
+  j++;
   exit(0); // To avoid cross-compiler exit routine
   return 0; // Never executed, just for compatibility
 }
